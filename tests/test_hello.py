f910cf0e9b3b2172f0c9add82a95aa214c6928a2
Matt Shin <matthew.shin@metoffice.gov.uk>
2019-12-02 10:20:00 +0000

    Lowercase world

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,5 +1,5 @@
 from hello import hello
 
 def test_hello():
-    assert hello() == 'Hello World! Hi!'
+    assert hello() == 'Hello world! Hi!'
     assert hello('Martians') == 'Hello Martians! Hi!'
